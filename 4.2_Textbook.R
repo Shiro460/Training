8b4d7a3e6639371037813e7098b46d1d5a611c3a
Shiro460 <35022509+Shiro460@users.noreply.github.com>
2018-01-06 19:24:05 +0900

    Chapter 4 Section 2
    Chapter 4 Section 2

--- 4.2_Textbook.R
+++ 4.2_Textbook.R
@@ -35,5 +35,30 @@ sms_corpus_clean <- tm_map(sms_corpus, content_transformer(tolower))
 as.character(sms_corpus[[1]])
 as.character(sms_corpus_clean[[1]])
 
-# SMSメッセージから数字を取り除く
+# SMSメッセージから数字やstopwordsを取り除く
 sms_corpus_clean <- tm_map(sms_corpus_clean, removeNumbers)
+sms_corpus_clean <- tm_map(sms_corpus_clean, removeWords, stopwords())
+
+# 記号類を削除(テキストのやり方)
+sms_corpus_clean <- tm_map(sms_corpus_clean, removePunctuation)
+
+# 記号を空白に置換(テキストのコラムのやり方)
+# 置換をするメソッドの作成
+replacePunctuation <- function(x) {
+  gsub("[[:punct:]]+", " ", x)
+}
+# 置換処理
+# sms_corpus_clean <- tm_map(sms_corpus_clean, replacePunctuation)
+
+# stemming処理
+# SnowballCパッケージのインストール(初回のみ)とロード
+# install.packages("SnowballC")
+library(SnowballC)
+sms_corpus_clean <- tm_map(sms_corpus_clean, stemDocument)
+
+# 余分なスペースの削除
+sms_corpus_clean <- tm_map(sms_corpus_clean, stripWhitespace)
+
+# 結果の確認
+as.character(sms_corpus[1:3])
+as.character(sms_corpus_clean[1:3])
